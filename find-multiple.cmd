32acff3241c8826b049a797021006d4fe4dfc2c4
Paul Melekhov <gugglegum@gmail.com>
2024-06-30 06:02:12 +0400

    Add CMD launcher for Windows to run like a system command

---
+++ find-multiple.cmd
@@ -0,0 +1,4 @@
+@echo off
+@rem Add the directory with this utility to the PATH environment variable and you will be able to call it
+@rem from any directory simply by "find-multiple" (like a system command)
+php %~dp0find-multiple.php %*
